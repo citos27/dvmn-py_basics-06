35db16ad46ce6377e6926c93943019481142c142
citos27 <bulanov200158@gmail.com>
2025-08-23 04:34:00 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+def is_very_long(password):
+    return len(password) > 12
+
+
+def has_digit(password):
+    return any(symbol.isdigit() for symbol in password)
+
+
+def has_lower_letters(password):
+    return any(symbol.islower() for symbol in password)
+
+
+def has_upper_letters(password):
+    return any(symbol.isupper() for symbol in password)
+
+
+def has_symbols(password):
+    return any(not symbol.isdigit() and not symbol.isalpha() for symbol in password)
+
+
+def main():
+    password = input("Введите пароль: ")
+    score = 0
+    functions = [
+        is_very_long(password),
+        has_digit(password),
+        has_lower_letters(password),
+        has_upper_letters(password),
+        has_symbols(password),
+    ]
+    for check in functions:
+        if check:
+            score = score + 2
+
+
+    print("Рейтинг пароля:", score)
+if __name__ == "__main__":
+    main()
